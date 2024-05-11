9278cbb3f89a65e9f6732a333663258f26668841
mircocosov <Artem22832@mail.ru>
2024-05-11 10:26:15 +0300

    feat: main block animation

--- src/pages/Home/HomeMain/HomeMain.tsx
+++ src/pages/Home/HomeMain/HomeMain.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import style from "./HomeMain.module.scss";
 import { carModificationsData } from "@/api/data/car-modification";
 
@@ -6,16 +7,36 @@ interface Props {
 }
 
 export default function HomeMain({ modificationId }: Props) {
+  const [currentModificationId, setCurrentModificationId] =
+    useState(modificationId);
+  useEffect(() => {
+    document
+      .getElementsByClassName(style.main__image)[0]
+      .classList.add(style.animated);
+    document
+      .getElementsByClassName(style.main__info_block)[0]
+      .classList.add(style.animated);
+    setTimeout(() => {
+      document
+        .getElementsByClassName(style.main__image)[0]
+        .classList.remove(style.animated);
+      document
+        .getElementsByClassName(style.main__info_block)[0]
+        .classList.remove(style.animated);
+
+      setCurrentModificationId(modificationId);
+    }, 1000);
+  }, [modificationId]);
+
   return (
     <div className={style.main__container}>
       <div className={style.main__container__prefix}>
         <span className={style.main__prefix}>ПРИОБРЕТИТЕ МАШИНУ МЕЧТЫ</span>
       </div>
-
       <div className={style.main__info_block_with_image}>
         <div className={style.main_image_container}>
           <img
-            src={carModificationsData[Number(modificationId) ?? 0].image}
+            src={carModificationsData[Number(currentModificationId) ?? 0].image}
             alt="Car"
             className={style.main__image}
           />
@@ -23,22 +44,43 @@ export default function HomeMain({ modificationId }: Props) {
         <div className={style.main__info_block}>
           <p>
             Объем двигателя :{" "}
-            {carModificationsData[Number(modificationId) ?? 0].engineVolume} л.
+            {
+              carModificationsData[Number(currentModificationId) ?? 0]
+                .engineVolume
+            }{" "}
+            л.
           </p>
           <p>
             Мощность:{" "}
-            {carModificationsData[Number(modificationId) ?? 0].enginePower} л.с.
+            {
+              carModificationsData[Number(currentModificationId) ?? 0]
+                .enginePower
+            }{" "}
+            л.с.
           </p>
           <p>
             Год выпуска:{" "}
-            {carModificationsData[Number(modificationId) ?? 0].year}
+            {carModificationsData[Number(currentModificationId) ?? 0].year}
           </p>
           <p>
             Разгон до 100 км/ч:{" "}
-            {carModificationsData[Number(modificationId) ?? 0].acceleration} с.
+            {
+              carModificationsData[Number(currentModificationId) ?? 0]
+                .acceleration
+            }{" "}
+            с.
           </p>
-          <p>Цвет: {carModificationsData[Number(modificationId) ?? 0].color}</p>
-          <p>Цена : {Intl.NumberFormat("ru-RU").format(carModificationsData[Number(modificationId) ?? 0].price)} ₽</p>
+          <p>
+            Цвет:{" "}
+            {carModificationsData[Number(currentModificationId) ?? 0].color}
+          </p>
+          <p>
+            Цена :{" "}
+            {Intl.NumberFormat("ru-RU").format(
+              carModificationsData[Number(currentModificationId) ?? 0].price
+            )}{" "}
+            ₽
+          </p>
         </div>
       </div>
     </div>
